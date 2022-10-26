45b9a78b33b99872d056a2a6ad1481f3c479afda
Etoile2003 <bastien.pelletier01@gmail.com>
2022-10-26 16:24:56 +0200

    debut de la creation des livres

---
+++ module/mod_Creation_Livre/vue_CLivre.php
@@ -0,0 +1,31 @@
+<?php
+require_once("vue_generique.php");
+class Vue_CLivre extends vueGenerique
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    public function form_ajout()
+    {
+    }
+
+    public function login()
+    {
+
+
+    }
+
+    public function signup()
+    {
+
+    
+
+    }
+}
+
+
+
+?>
