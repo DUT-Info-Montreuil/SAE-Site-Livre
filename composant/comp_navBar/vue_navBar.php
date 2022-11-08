2ac0cc54da25c09e08df9d8bb1664b2692d95446
Armandpro12 <chaplainduretarmandpro@gmail.com>
2022-11-08 22:22:47 +0100

    ajout des icones dans vue_navBar.php

--- composant/comp_navBar/vue_navBar.php
+++ composant/comp_navBar/vue_navBar.php
@@ -1,21 +1,37 @@
 <?php
 require_once("vue_generique.php");
+
 class VueNavBar extends VueGenerique
 {
     public function __construct()
     {
         parent::__construct();
     }
+
     public function printnavBar()
     {
-?>
+        ?>
         <html>
         <header class="p-3 text-bg-dark">
+            <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                <symbol id="powerIcon" viewBox="0 0 16 16">
+                    <path d="M7.5 1v7h1V1h-1z"/>
+                    <path d="M3 8.812a4.999 4.999 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812z"/>
+                </symbol>
+
+                <symbol id="bookIcon" viewBox="0 0 16 16">
+                    <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
+                </symbol>
+
+                <symbol id="pencilFillIcon" viewBox="0 0 16 16">
+                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                </symbol>
+            </svg>
             <div class="container">
                 <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                     <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
                         <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
-                            <use xlink:href="#bootstrap" />
+                            <use xlink:href="#bootstrap"/>
                         </svg>
                     </a>
 
@@ -28,32 +44,55 @@ class VueNavBar extends VueGenerique
                     </ul>
 
                     <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-                        <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search">
+                        <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..."
+                               aria-label="Search">
                     </form>
                     <?php
                     if (isset($_SESSION["connected"])) {
-                    ?>
+                        ?>
                         <div class="dropdown">
-                            <button id="ppDropDown" class="rounded-circle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <button id="ppDropDown" class="rounded-circle" type="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
                             </button>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><hr class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="index.php?module=connexion&action=disconnect">deconnexion</a></li>
+                                <li><a class="dropdown-item" href="#">
+                                        <svg class="bi-pencil-fill" width="16" height="16">
+                                            <use xlink:href="#pencilFillIcon"/>
+                                        </svg>
+                                        Action
+                                    </a></li>
+                                <li>
+                                    <a class="dropdown-item d-flex gap-2 align-items-center" href="#">
+                                        <svg class="bi bi-book" width="16" height="16">
+                                            <use xlink:href="#bookIcon"/>
+                                        </svg>
+                                        other action
+                                    </a>
+                                </li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item d-flex gap-2 align-items-center"
+                                       href="index.php?module=connexion&action=disconnect">
+                                        <svg class="bi" width="16" height="16">
+                                            <use xlink:href="#powerIcon"/>
+                                        </svg>
+                                        deconnexion
+                                    </a>
+                                </li>
                             </ul>
                         </div>
 
 
-
-                    <?php
+                        <?php
 
                     } else { ?>
                         <div class="text-end">
                             <!-- <a href="index.php?action=print_signup&module=connexion" class="btn btn-outline-light me-2" role="button">sign up</a>  -->
-                            <a href="index.php?action=print_login&module=connexion" class="btn btn-warning" role="button">Login</a>
+                            <a href="index.php?action=print_login&module=connexion" class="btn btn-warning"
+                               role="button">Login</a>
                         </div>
-                    <?php
+                        <?php
                     }
                     ?>
 
@@ -62,7 +101,8 @@ class VueNavBar extends VueGenerique
         </header>
 
         </html>
-<?php
+        <?php
     }
 }
+
 ?>
