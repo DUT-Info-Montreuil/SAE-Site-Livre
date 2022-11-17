aa4db54109694ac06626e01abf1f2977e6b5cedf
Armandpro12 <chaplainduretarmandpro@gmail.com>
2022-11-17 20:19:09 +0100

    test de mettre JQuery dans un fichier à part mais ça ne marche pas donc tant pis. début d'ajout d'un script jQuery pour changement de nom (pour l'instant marche pour passer de afficherNom à modifierNom)

--- module/mod_profil/vue_profil.php
+++ module/mod_profil/vue_profil.php
@@ -140,8 +140,6 @@ class vue_profil extends vueGenerique
                                         </div>
                                     </div>
                                 </div>
-
-
                                 <script>
                                     $("#btnHistoryRead").click(function () {
                                         $("#read-list-history").removeClass("d-none");
@@ -166,13 +164,12 @@ class vue_profil extends vueGenerique
                                     <div class="col-sm-3">
                                         <p class="mb-1 ms-4">Nom</p>
                                     </div>
-                                    <?php
-                                    if (isset($_GET["changeNameClicked"]) && $_GET["changeNameClicked"] == "true") {
+                                   <?php /*
+                                    if (isset($_GET["changeNameClicked"]) && $_GET["changeNameClicked"] == "true") { */
                                         ?>
-                                        <div class="col-sm-5">
+                                        <div class="col-sm-5 d-none" id="modifierNom">
                                             <form class="row g-2" action="index.php?module=profil&action=modifierNom"
                                                   method="post">
-
                                                 <div class="col-auto">
                                                     <input type="text" class="form-control" placeholder="new name"
                                                            name="newName" required>
@@ -200,29 +197,42 @@ class vue_profil extends vueGenerique
                                                 </div>
                                             </form>
                                         </div>
-                                        <?php
-                                    } else {
+                                        <?php /*
+                                    } else {*/
                                         ?>
-                                        <div class="col-sm-4">
-                                            <p id="name" class="text-muted mb-0">
-                                                <?php echo $_SESSION["identifiant"] ?>
-                                            </p>
-                                        </div>
-                                        <div class="col-sm-1">
-                                            <a role="button" id="swapToChangeNameButton" type="button"
-                                               class="btn btn-outline-secondary ms-0"
-                                               href="index.php?module=profil&action=afficherProfil&changeNameClicked=true">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                     fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-                                                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-                                                </svg>
-                                            </a>
-                                        </div>
+                                            <div class="col-sm-4 afficherNom">
+                                                <p id="name" class="text-muted mb-0">
+                                                    <?php echo $_SESSION["identifiant"] ?>
+                                                </p>
+                                            </div>
+                                            <div class="col-sm-1 afficherNom">
+                                                <button id="swapToChangeNameButton" type="button"
+                                                   class="btn btn-outline-secondary ms-0">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                         fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                                                        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                                                    </svg>
+                                                </button>
+                                            </div>
                                         <?php
-                                    }
+                                    //}
                                     ?>
                                 </div>
                             <hr>
+                                <script>
+                                    $("#swapToChangeNameButton").click(function () {
+                                        $(".afficherNom").addClass("d-none");
+                                        $("#modifierNom").removeClass("d-none");
+                                    });
+                                    $("#cancelChangeNameButton").click(function () {
+                                        $(".afficherNom").removeClass("d-none");
+                                        $("#modifierNom").addClass("d-none");
+                                    });
+                                    $("#ConfirmChangeNameButton").click(function () {
+                                        $(".afficherNom").removeClass("d-none");
+                                        $("#modifierNom").addClass("d-none");
+                                    });
+                                </script>
                             <div class="row">
                                 <div class="col-sm-3">
                                     <p class="mb-1 ms-4">Email</p>
