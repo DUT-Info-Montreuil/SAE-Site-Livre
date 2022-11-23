c0c59ce89b1eda26f98193d83752c8f43e46e900
THEfireZz <maximedhaeyere@gmail.com>
2022-11-23 12:05:29 +0100

    Affichage des livre individuellement quand il est sélectionné dans la bibliothèque

--- module/mod_bibliothèque/modele_bibliotheque.php
+++ module/mod_bibliothèque/modele_bibliotheque.php
@@ -8,25 +8,30 @@ class Modele_Biblio extends Connexion
     public function getLivres()
     {
 
-        $sql = "SELECT Livre.id, titre,resumeLivre,nbrLike,nbrVue,userName
-                FROM Livre
-                INNER JOIN Utilisateur
-                ON Livre.IDAuteur = Utilisateur.id";
+        $sql = "SELECT  Livre.id, titre,resumeLivre,nbrLike,nbrVue,userName, GROUP_CONCAT(genre SEPARATOR ',') as genres
+        FROM Livre
+        INNER JOIN Utilisateur
+        ON Livre.IDAuteur = Utilisateur.id
+        inner join LivreGenre
+        ON LivreGenre.idLivre=Livre.id
+        INNER JOIN Genre
+        ON LivreGenre.idGenre=Genre.id
+        GROUP BY Livre.id";
         $prepare = parent::$bdd->prepare($sql);
         $exec = $prepare->execute();
         $result = $prepare->fetchAll();
         return $result;
     }
 
-    /*   public function getGenres($idLivre){
+     /*  public function getGenres($idLivre){
         $sql = "SELECT genre FROM Genre inner join LivreGenre on LivreGenre.idGenre = Genre.id WHERE LivreGenre.idLivre=$idLivre LIMIT 3  ";
         $prepare = parent::$bdd->prepare($sql);
         $exec = $prepare->execute(); 
         $result = $prepare->fetchAll();
         return $result;
-    }
+    }*/
 
-*/
+
     
 }
 
