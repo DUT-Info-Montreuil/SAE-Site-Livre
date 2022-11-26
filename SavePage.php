4ccd3c454b019d9d3e17cb549a32defb89aabc22
Etoile2003 <95474735+Etoile2003@users.noreply.github.com>
2022-11-26 10:53:52 +0000

    chamgement update to insert

--- SavePage.php
+++ SavePage.php
@@ -13,8 +13,8 @@ class SavePage extends Connexion{
         $exec = $prepare->execute(array($idPage , $idChapitre , $numPage));
         $result = $prepare->fetchAll();
         
-        $prepare2 = parent::$bdd->prepare("UPDATE Page SET TexteDeLaPage = ? WHERE ID = ? ;");
-        $exec2 = $prepare2->execute(array($result[0]["TexteDeLaPage"] , $idPage));
+        $prepare2 = parent::$bdd->prepare("INSERT INTO Page (ID , numeroPage , TesteDeLaPage , id_chapitre) VALUES(? , ? , ? , ?) ON DUPLICATE KEY UPDATE TexteDeLaPage = ? WHERE ID = ? ;");
+        $exec2 = $prepare2->execute(array( $idPage,$numPage,$result[0]["TexteDeLaPage"],$idChapitre,$result[0]["TexteDeLaPage"] , $idPage));
 
         $prepare3 = parent::$bdd->prepare("DELETE FROM TempSave WHERE idPage = ? AND id_chapitre = ? AND numeroPage = ? ;");
         $exec3 = $prepare3->execute(array($idPage , $idChapitre , $numPage));
