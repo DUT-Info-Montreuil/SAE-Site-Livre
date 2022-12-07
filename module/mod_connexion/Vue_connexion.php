4f4618a63817846e9afb9bc6e602ef5684fdaa27
THEfireZz <maximedhaeyere@gmail.com>
2022-12-07 15:37:59 +0100

    Modification des visuels de la bibliotheque et de la nav bar

--- module/mod_connexion/Vue_connexion.php
+++ module/mod_connexion/Vue_connexion.php
@@ -16,7 +16,7 @@ class Vue_connexion extends vueGenerique
     {
 
 ?>
-        
+
 
 
         <div class="container d-flex flex-column">
@@ -27,7 +27,11 @@ class Vue_connexion extends vueGenerique
                         <!-- Card body -->
                         <div class="card-body p-6">
                             <div class="mb-4">
-                                <a href="index.php?module=accueil"><img src="ressource/livreTest.png" class="mb-4" alt="" /></a>
+                                <a href="index.php?module=accueil" class="text-dark"><svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+                                        <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z" />
+                                        <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6Z" />
+                                    </svg>
+                                </a>
                                 <h1 class="mb-1 fw-bold">Connecte toi</h1>
                                 <span>Tu n'as pas de compte?
                                     <a href="index.php?action=print_signup&module=connexion" class="ms-1">Créé en un !</a></span>
@@ -69,7 +73,7 @@ class Vue_connexion extends vueGenerique
 
             </form>-->
 
-       
+
 
     <?php
 
@@ -90,7 +94,12 @@ class Vue_connexion extends vueGenerique
                         <!-- Card body -->
                         <div class="card-body p-6">
                             <div class="mb-4">
-                                <a href="index.php?module=accueil"><img src="ressource/livreTest.png" class="mb-4" alt="" /></a>
+                                <a href="index.php?module=accueil " class="text-dark">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+                                        <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z" />
+                                        <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6Z" />
+                                    </svg>
+                                </a>
                                 <h1 class="mb-1 fw-bold">Inscription</h1>
                                 <span>Tu as déja un compte?
                                     <a href="index.php?action=print_login&module=connexion" class="ms-1">Connecte toi !</a></span>
