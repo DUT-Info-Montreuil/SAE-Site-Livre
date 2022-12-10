c987da34b724a3bbcb15a3aa7da65bd3da79a44b
Etoile2003 <bastien.pelletier01@gmail.com>
2022-12-10 16:29:19 +0100

    creation chapitre

--- module/mod_Creation_Livre/Modele_CLivre.php
+++ module/mod_Creation_Livre/Modele_CLivre.php
@@ -133,9 +133,11 @@ class Modele_CLivre extends Connexion {
         $prepareTemp = parent::$bdd->prepare("SELECT TexteDeLaPage FROM TempSave where id_chapitre = ? and idPage = ?;");
         $execTemp = $prepareTemp->execute($arr);
         $resultTemp = $prepareTemp->fetch();
+
         if ($resultTemp["TexteDeLaPage"] != ""){
             return $resultTemp["TexteDeLaPage"];
         }
+       
 
         $prepare = parent::$bdd->prepare("SELECT TexteDeLaPage FROM Page where id_Chapitre = ? and ID = ?");
         $exec = $prepare->execute($arr);
