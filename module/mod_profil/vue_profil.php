0ba4aeb4872377bf0a240c235a7a5108b1a5f5f7
Armandpro12 <chaplainduretarmandpro@gmail.com>
2023-01-18 20:29:40 +0100

    correction grammaire dans vue_profil.php

--- module/mod_profil/vue_profil.php
+++ module/mod_profil/vue_profil.php
@@ -63,7 +63,7 @@ class vue_profil extends vueGenerique
                                     },
                                 }).done(function (data) {
                                     if (<?=$_SESSION['expiration_jeton']?> < Date.now().toString().slice(0, 10)) {
-                                        alert("cela fait plus de 10 minutes que vous êtes sur la page, à des fins de sécurité, veuillez actualiser la page");
+                                        alert("Cela fait plus de 10 minutes que vous êtes sur la page, à des fins de sécurité, veuillez actualiser la page");
                                     } else {
                                         $("#suivre").first().fadeOut("speed", function fadeNext() {
                                             $("#img-check-suivi").hide();
@@ -88,7 +88,7 @@ class vue_profil extends vueGenerique
                                     },
                                 }).done(function (data) {
                                     if (<?=$_SESSION['expiration_jeton']?> < Date.now().toString().slice(0, 10)) {
-                                        alert("cela fait plus de 10 minutes que vous êtes sur la page, à des fins de sécurité, veuillez actualiser la page");
+                                        alert("Cela fait plus de 10 minutes que vous êtes sur la page, à des fins de sécurité, veuillez actualiser la page");
                                     } else {
                                         $("#img-check-suivi").slideUp();
                                         $("#suivi").fadeOut("speed", function fadeNext() {
@@ -131,7 +131,7 @@ class vue_profil extends vueGenerique
                                                                     ?>
                                                                     <a type="button"
                                                                        href="index.php?module=livre&idLivre=<?= $key["id"] ?>&Chapitre=<?= $key2["dernier_chapitre_lu"] ?>"
-                                                                       class="btn btn-outline-dark">reprendre la
+                                                                       class="btn btn-outline-dark">Reprendre la
                                                                         lecture</a>
                                                                     <div class="card-bottom">
                                                                         <p class="card-text"><small class="text-muted">lu
@@ -148,7 +148,7 @@ class vue_profil extends vueGenerique
                                                                 ?>
                                                                 <a type="button"
                                                                    href="index.php?module=livre&idLivre=<?= $key["id"] ?>&Chapitre=1"
-                                                                   class="btn btn-outline-dark">commencer la
+                                                                   class="btn btn-outline-dark">Commencer la
                                                                     lecture</a>
                                                                 <?php
                                                             }
@@ -167,7 +167,7 @@ class vue_profil extends vueGenerique
                                     <div class="card-bottom text-center mb-2">
                                         <a href="index.php?module=historique&action=historiqueEcriture&id=<?= htmlspecialchars($_GET['id']) ?>"
                                            type="button"
-                                           class="btn btn-outline-secondary">voir plus</a>
+                                           class="btn btn-outline-secondary">Voir plus</a>
                                     </div>
                                 </div>
                             </div>
@@ -242,7 +242,7 @@ class vue_profil extends vueGenerique
                                            autocomplete="off">
                                     <label class="btn btn-outline-secondary" for="btnHistoryWrite"
                                            id="btnHistoryWriteLabel">
-                                        Ecriture
+                                        Écriture
                                     </label>
                                 </div>
                                 <div class="list-group" id="read-list-history">
@@ -267,7 +267,7 @@ class vue_profil extends vueGenerique
                                                             <p class="card-text"><?= $key["resumeLivre"] ?></p>
                                                             <a type="button"
                                                                href="index.php?module=livre&idLivre=<?= $key["idLivre"] ?>&Chapitre=<?= $key["dernier_chapitre_lu"] ?>"
-                                                               class="btn btn-outline-dark">reprendre la lecture</a>
+                                                               class="btn btn-outline-dark">Reprendre la lecture</a>
                                                         </div>
                                                         <div class="card-bottom">
                                                             <p class="card-text"><small
@@ -285,13 +285,13 @@ class vue_profil extends vueGenerique
                                             <?php
                                         }
                                     } else {
-                                        echo "vous n'avez pas encore lu de livres";
+                                        echo "Vous n'avez pas encore lu de livres";
                                     }
 
                                     ?>
                                     <div class="card-bottom text-center mb-2">
                                         <a href="index.php?module=historique&action=historiqueLecture" type="button"
-                                           class="btn btn-outline-secondary">voir plus</a>
+                                           class="btn btn-outline-secondary">Voir plus</a>
                                     </div>
                                 </div>
 
@@ -317,7 +317,7 @@ class vue_profil extends vueGenerique
                                                             <p class="card-text"><?= $key["resumeLivre"] ?></p>
                                                             <a type="button"
                                                                href="index.php?module=CLivre&action=menu_write_book&idLivre=<?=$key['id']?>"
-                                                               class="btn btn-outline-dark">reprendre l'écriture</a>
+                                                               class="btn btn-outline-dark">Reprendre l'écriture</a>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -325,14 +325,14 @@ class vue_profil extends vueGenerique
                                             <?php
                                         }
                                     } else {
-                                        echo "vous n'avez pas encore écrit de livres";
+                                        echo "Vous n'avez pas encore écrit de livres";
                                     }
 
                                     ?>
                                     <div class="card-bottom text-center mb-2">
                                         <a href="index.php?module=historique&action=historiqueEcriture&id=<?= $_SESSION['id'] ?>"
                                            type="button"
-                                           class="btn btn-outline-secondary">voir plus</a>
+                                           class="btn btn-outline-secondary">Voir plus</a>
                                     </div>
                                 </div>
                                 <script>
@@ -419,7 +419,7 @@ class vue_profil extends vueGenerique
                                                     },
                                                 }).done(function (data) {
                                                     if (<?=$_SESSION['expiration_jeton']?> < Date.now().toString().slice(0, 10)) {
-                                                        alert("cela fait plus de 10 minutes que vous êtes sur la page, à des fins de sécurité, veuillez actualiser la page");
+                                                        alert("Cela fait plus de 10 minutes que vous êtes sur la page, à des fins de sécurité, veuillez actualiser la page");
                                                     } else {
                                                         if (data.length < 100) {
                                                             $("#name").text($("#inputName").val());
@@ -507,7 +507,7 @@ class vue_profil extends vueGenerique
                                                     },
                                                 }).done(function (data) {
                                                     if (<?=$_SESSION['expiration_jeton']?> < Date.now().toString().slice(0, 10)) {
-                                                        alert("cela fait plus de 10 minutes que vous êtes sur la page, à des fins de sécurité, veuillez actualiser la page");
+                                                        alert("Cela fait plus de 10 minutes que vous êtes sur la page, à des fins de sécurité, veuillez actualiser la page");
                                                     } else {
                                                         if (data.length < 100) {
                                                             $("#email").text($("#inputEmail").val());
@@ -592,7 +592,7 @@ class vue_profil extends vueGenerique
                                                     },
                                                 }).done(function (data) {
                                                     if (<?=$_SESSION['expiration_jeton']?> < Date.now().toString().slice(0, 10)) {
-                                                        alert("cela fait plus de 10 minutes que vous êtes sur la page, à des fins de sécurité, veuillez actualiser la page");
+                                                        alert("Cela fait plus de 10 minutes que vous êtes sur la page, à des fins de sécurité, veuillez actualiser la page");
                                                     } else {
                                                         if (data.length < 100) {
                                                             $("#password").text($("#inputMDP").val());
@@ -622,7 +622,7 @@ class vue_profil extends vueGenerique
                                 <?php
                                 if (count($mesAbonnements) === 0) {
 
-                                    echo "vous n'êtes abonné à personne";
+                                    echo "Vous n'êtes abonné à personne";
                                 }
                                 foreach ($mesAbonnements as $key) {
                                     ?>
